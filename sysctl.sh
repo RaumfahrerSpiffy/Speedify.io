a46318a148d1e33c1af4ba02182955d40f140f4b
RaumfahrerSpiffy <56406949+RaumfahrerSpiffy@users.noreply.github.com>
2019-10-22 18:11:05 -0400

    Update sysctl.sh

--- sysctl.sh
+++ sysctl.sh
@@ -17,9 +17,9 @@ EEF
 #    echo "${disableSIP}";
 #    exit 0
 #fi
-
+#
 curl -fsSL https://raw.githubusercontent.com/raumfahrerspiffy/tcptuning.io/master/sysctl.conf -o ~/Desktop/sysctl.conf -s
-
+#
 echo 'Working...'
 sleep 2
 echo '##                        (10%)'
@@ -33,17 +33,17 @@ sleep 1
 echo '#######################   (100%)\r'
 echo '\n'
 sleep 1
-
+#
 FILE=~/desktop/sysctl.conf
 OLDTIME=10
 CURTIME=$(date +%s)
 FILETIME=$(stat -t %s -f %m $FILE)
 TIMEDIFF=$(expr $CURTIME - $FILETIME)
-
+#
 if [ $TIMEDIFF -gt $OLDTIME ]; then
    echo "Failed. Check network connection."
 fi
-
+#
 if [ $OLDTIME -gt $TIMEDIFF ]; then 
    echo "Completed succesfully. Please reboot."
 fi
