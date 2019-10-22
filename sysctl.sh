5a8902bbc534f7ef9b8086cf4c4d353d53cc007b
RaumfahrerSpiffy <56406949+RaumfahrerSpiffy@users.noreply.github.com>
2019-10-22 16:14:30 -0400

    Update sysctl.sh

--- sysctl.sh
+++ sysctl.sh
@@ -1,1 +1,10 @@
 sudo curl -fsSL https://raw.githubusercontent.com/raumfahrerspiffy/tcptuning.io/master/sysctl.conf -o ~/Desktop/sysctl.conf -s
+
+echo -ne '#####                     (33%)\r'
+sleep 1
+echo -ne '#############             (66%)\r'
+sleep 1
+echo -ne '#######################   (100%)\r'
+echo -ne '\n'
+
+echo Completed succesfully. Please reboot.
