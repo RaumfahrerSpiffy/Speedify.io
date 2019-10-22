d2108e906352ae608f817ae3b5d824d6132c8097
RaumfahrerSpiffy <56406949+RaumfahrerSpiffy@users.noreply.github.com>
2019-10-22 18:36:00 -0400

    Update sysctl.sh

--- sysctl.sh
+++ sysctl.sh
@@ -6,7 +6,7 @@ EEF
 #
 SIPchecker=($(csrutil status | awk '{ print $5 }'))
 if [ "$SIPchecker" != "disabled." ]; then
-    echo "Disable System Integrity Protection, then re-run script."
+    echo "Disable SYSTEM INTEGRITY PROTECTION, then re-run script."
     echo "Refer to README for help.";
     exit 0
 fi
