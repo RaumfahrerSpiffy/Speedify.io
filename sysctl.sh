2ebe850afd144292b0d04b9855909c865188ccbf
RaumfahrerSpiffy <56406949+RaumfahrerSpiffy@users.noreply.github.com>
2019-10-22 18:58:18 -0400

    Update sysctl.sh

--- sysctl.sh
+++ sysctl.sh
@@ -39,7 +39,7 @@ CURTIME=$(date +%s)
 FILETIME=$(stat -t %s -f %m $FILE)
 TIMEDIFF=$(expr $CURTIME - $FILETIME)
 #
-if [ $FILENAME | "No such file or directory" ]; then
+if [ $FILENAME == "No such file or directory" ]; then
    echo "Failed. Check network connection."
    exit 0
 fi
