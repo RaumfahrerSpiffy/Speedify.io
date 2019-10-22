2b86ed593de87b860659d37b558b354e1a9815c5
RaumfahrerSpiffy <56406949+RaumfahrerSpiffy@users.noreply.github.com>
2019-10-22 17:17:31 -0400

    Update sysctl.sh

--- sysctl.sh
+++ sysctl.sh
@@ -2,12 +2,6 @@
 
 #sipChecker=sudo csrutil status
 #disableSIP=Please disable SIP and re-run script.
-FILE=~/desktop/sysctl.conf
-OLDTIME=10
-CURTIME=$(date +%s)
-FILETIME=$(stat -t %s -f %m $FILE)
-TIMEDIFF=$(expr $CURTIME - $FILETIME)
-
 #if [ "${sipChecker}" != "disabled." ]; then
 #    echo "${disableSIP}";
 #    exit 0
@@ -17,7 +11,6 @@ curl -fsSL https://raw.githubusercontent.com/raumfahrerspiffy/tcptuning.io/maste
 
 echo 'Working...'
 sleep 2
-
 echo '##                        (10%)'
 sleep 1
 echo '#####                     (33%)\r'
@@ -28,6 +21,12 @@ echo '##################        (78%)\r'
 sleep 1
 echo '#######################   (100%)\r'
 echo '\n'
+
+FILE=~/desktop/sysctl.conf
+OLDTIME=10
+CURTIME=$(date +%s)
+FILETIME=$(stat -t %s -f %m $FILE)
+TIMEDIFF=$(expr $CURTIME - $FILETIME)
 
 if [ $TIMEDIFF -gt $OLDTIME ]; then
    echo "Failed, check network connection."
