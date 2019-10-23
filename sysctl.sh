aed5173cc00b43f132d0d4e62d46091ec4dbdc80
RaumfahrerSpiffy <56406949+RaumfahrerSpiffy@users.noreply.github.com>
2019-10-22 20:31:07 -0400

    Update sysctl.sh

--- sysctl.sh
+++ sysctl.sh
@@ -16,6 +16,8 @@ EEF
 #    exit 0
 #fi
 #
+echo "PLEASE AUTHENTICATE"
+#
 sudo curl -fsSL https://raw.githubusercontent.com/raumfahrerspiffy/tcptuning.io/master/sysctl.conf -o /etc/sysctl.conf -s
 #
 echo "\n"
